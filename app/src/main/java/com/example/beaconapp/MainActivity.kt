3d78063855448788108f01371d560c9c8dc69e12
erisa <erisa.hoxha@ffuf.de>
2022-06-13 20:52:40 +0200

    temperature fix and layout fix

--- app/src/main/java/com/example/beaconapp/MainActivity.kt
+++ app/src/main/java/com/example/beaconapp/MainActivity.kt
@@ -3,17 +3,17 @@ package com.example.beaconapp
 import android.Manifest
 import android.annotation.SuppressLint
 import android.bluetooth.*
-import android.bluetooth.le.*
-import android.content.ContentValues
-import android.content.Context
-import android.os.*
+import android.bluetooth.le.BluetoothLeScanner
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.RemoteException
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import org.altbeacon.beacon.*
 import org.altbeacon.beacon.utils.UrlBeaconUrlCompressor
-import java.util.*
 
 
 const val DEVICE_MAC_ADDRESS = "F8:20:74:F7:2B:82"
@@ -43,112 +43,24 @@ class MainActivity : AppCompatActivity(), BeaconConsumer, RangeNotifier {
         checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
         checkSelfPermission(Manifest.permission.BLUETOOTH_SCAN)
 
-        if (mBluetoothManager == null) {
-            mBluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-            if (mBluetoothManager == null) {
-                Log.e(ContentValues.TAG, "Unable to initialize BluetoothManager.")
-            }
-        }
-
-        mBluetoothAdapter = mBluetoothManager!!.adapter
-        if (mBluetoothAdapter == null) {
-            Log.e(ContentValues.TAG, "Unable to obtain a BluetoothAdapter.")
-        }
-        if (mBluetoothAdapter != null) {
-            bluetoothLeScanner = mBluetoothAdapter!!.bluetoothLeScanner
-
-            if (bluetoothLeScanner == null) {
-                Log.e(ContentValues.TAG, "Unable to obtain a BluetoothAdapter.")
-            }
-        }
-
         beaconManager =  BeaconManager.getInstanceForApplication(this)
 //        val region = Region("all-beacons-region", null, null, null)
+
+        // Detect the main identifier (UID) frame:
+        beaconManager?.getBeaconParsers()!!
+            .add(BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT))
+
+        // Detect the URL frame:
         beaconManager?.getBeaconParsers()!!
             .add(BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_URL_LAYOUT))
+
+        // Detect the telemetry (TLM) frame:
         beaconManager?.getBeaconParsers()!!
             .add(BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_TLM_LAYOUT))
         beaconManager?.bind(this)
 
-        scanLeDevice()
-
-        update_data.setOnClickListener{
-            print("erisa")
-        }
 
     }
-
-    @SuppressLint("MissingPermission")
-    private fun scanLeDevice() {
-        if (!scanning) { // Stops scanning after a pre-defined scan period.
-            handler.postDelayed({
-                scanning = false
-                bluetoothLeScanner!!.stopScan(leScanCallback)
-            }, SCAN_PERIOD)
-            scanning = true
-
-            val filters: MutableList<ScanFilter> = ArrayList()
-            val filter = ScanFilter.Builder()
-                .setDeviceAddress(DEVICE_MAC_ADDRESS)
-                .build()
-            filters.add(filter)
-            val settings = ScanSettings.Builder()
-                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
-                .build()
-
-            bluetoothLeScanner!!.startScan(filters, settings, leScanCallback)
-
-
-        } else {
-            scanning = false
-            bluetoothLeScanner!!.stopScan(leScanCallback)
-        }
-        invalidateOptionsMenu()
-    }
-
-    // Device scan callback.
-    private val leScanCallback: ScanCallback = object : ScanCallback() {
-        @SuppressLint("MissingPermission")
-        override fun onScanResult(callbackType: Int, result: ScanResult) {
-            super.onScanResult(callbackType, result)
-            Log.i(ContentValues.TAG, "Hello $result")
-
-            val scanRecord = result.scanRecord
-            val beacon = Beacon(result.device.address)
-            beacon.manufacturer = result.device.name
-            beacon.rssi = result.rssi
-            if (scanRecord != null) {
-
-                val serviceUuids = scanRecord.serviceUuids
-                if (serviceUuids != null && serviceUuids.size > 0 && serviceUuids.contains(
-                        ParcelUuid.fromString(SERVICE_UUID)
-                    )
-                ) {
-                    val serviceData = scanRecord.getServiceData(ParcelUuid.fromString(SERVICE_UUID))
-                    if (serviceData != null && serviceData.size > 18) {
-                        val eddystoneUUID =
-                            toHexString(Arrays.copyOfRange(serviceData, 2, 18))
-                        val namespace = String(eddystoneUUID.toCharArray().sliceArray(0..19))
-                        val instance = String(
-                            eddystoneUUID.toCharArray()
-                                .sliceArray(20 until eddystoneUUID.toCharArray().size)
-                        )
-                        beacon.type = Beacon.beaconType.eddystoneUID
-                        beacon.namespace = namespace
-                        beacon.instance = instance
-
-                        beacon_id.text = namespace + instance
-                    }
-                }
-
-            }
-
-//            beaconSet.add(beacon)
-//            (recyclerView.adapter as BeaconsAdapter).updateData(beaconSet.toList(),beaconTypePositionSelected)
-        }
-
-        }
-
 
     private val bluetoothGattCallback = object : BluetoothGattCallback() {
         @SuppressLint("MissingPermission")
@@ -166,12 +78,6 @@ class MainActivity : AppCompatActivity(), BeaconConsumer, RangeNotifier {
         @SuppressLint("MissingPermission")
         override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
             super.onServicesDiscovered(gatt, status)
-//            temperatureCharacteristic = gatt?.getService(UUID.fromString(DEVICE_UIID))
-//                ?.getCharacteristic(UUID.fromString(TEMP_CHARACTERISTIC))
-//            humidityCharacteristic = gatt?.getService(UUID.fromString(DEVICE_UIID))
-//                ?.getCharacteristic(UUID.fromString(HUMIDITY_CHARACTERISTIC))
-//
-//            enableButtons()
 
         }
 
@@ -183,38 +89,11 @@ class MainActivity : AppCompatActivity(), BeaconConsumer, RangeNotifier {
         ) {
             super.onCharacteristicRead(gatt, characteristic, status)
             if (characteristic != null) {
-//                if(characteristic.uuid.toString() == TEMP_CHARACTERISTIC){
-//                    var result = characteristic.getFloatValue(52,1)
-//
-//                    tv_temp_value.text = "$result°C";
-//                }
-//                else if(characteristic.uuid.toString() == HUMIDITY_CHARACTERISTIC){
-//
-//                    val result = characteristic.getIntValue(18,0)
-//
-//                    var degrees = result / 100
-//                    var points = result % 100
-//
-//                    tv_humidity_value.text = "$degrees.$points%";
-//                }
+
             }
         }
 
 
-    }
-
-    private val HEX = "0123456789ABCDEF".toCharArray()
-    fun toHexString(bytes: ByteArray): String {
-        if (bytes.isEmpty()) {
-            return ""
-        }
-        val hexChars = CharArray(bytes.size * 2)
-        for (j in bytes.indices) {
-            val v = (bytes[j].toInt() and 0xFF)
-            hexChars[j * 2] = HEX[v ushr 4]
-            hexChars[j * 2 + 1] = HEX[v and 0x0F]
-        }
-        return String(hexChars)
     }
 
     override fun onBeaconServiceConnect() {
@@ -236,48 +115,24 @@ class MainActivity : AppCompatActivity(), BeaconConsumer, RangeNotifier {
             if (beacon.serviceUuid === 0xfeaa && beacon.beaconTypeCode === 0x10) {
                 // This is a Eddystone-URL frame
                 val url = UrlBeaconUrlCompressor.uncompress(beacon.id1.toByteArray())
-                beacon_url.text = "URL: " + url
-                beacon_voltage.text = "Voltage: " + beacon.extraDataFields.get(1)
-//                var result = characteristic.getFloatValue(52,1)
-//
-//                tv_temp_value.text = "$result°C";
-                beacon_temperature.text = "Temp: " + beacon.extraDataFields.get(2)
-                beacon_distance.text = "Dist: " + beacon.distance
-
-                for (kot in beacon.extraDataFields){
-                    Log.i("Kot", "$kot")
-                }
-
-                Log.i(
-                    "TAG", "I see a beacon transmitting a url: " + url +
-                            " approximately " + beacon.distance + " meters away."
-                )
+                beacon_url.text = url
             }
 
             if (beacon.serviceUuid === 0xfeaa && beacon.beaconTypeCode === 0x00) {
                 // This is a Eddystone-UID frame
                 val namespaceId = beacon.id1
                 val instanceId = beacon.id2
-                Log.d(
-                    "TAG", "I see a beacon transmitting namespace id: " + namespaceId +
-                            " and instance id: " + instanceId +
-                            " approximately " + beacon.distance + " meters away."
-                )
 
-                // Do we have telemetry data?
-                if (beacon.extraDataFields.size > 0) {
-                    val telemetryVersion = beacon.extraDataFields[0]
-                    val batteryMilliVolts = beacon.extraDataFields[1]
-                    val pduCount = beacon.extraDataFields[3]
-                    val uptime = beacon.extraDataFields[4]
-                    beacon_voltage.text = "voltage" + batteryMilliVolts
-                    Log.i(
-                        "TAG", "The above beacon is sending telemetry version " + telemetryVersion +
-                                ", has been up for : " + uptime + " seconds" +
-                                ", has a battery level of " + batteryMilliVolts + " mV" +
-                                ", and has transmitted " + pduCount + " advertisements."
-                    )
-                }
+                beacon_instanceId.text = "InstanceId: " + instanceId.toString()
+                beacon_namespaceId.text = "NamespaceId:" + namespaceId.toString()
+
+                beacon_voltage.text = beacon.extraDataFields.get(1).toString() + " V"
+                beacon_distance.text = beacon.distance.toString() + " metres"
+                val unsignedTemp = beacon.extraDataFields[2] shr 8
+                val temperature =
+                    if (unsignedTemp > 128) (unsignedTemp - 256).toDouble() else unsignedTemp + (beacon.extraDataFields[2] and 0xff) / 256.0
+                beacon_temperature.text = temperature.toString() + ""
+
             }
         }
     }
